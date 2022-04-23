15bc2f18e5c7fcf9e73cc70a715d4394ec2b5917
Seth Martin <jahgpup@gmail.com>
2022-04-23 10:36:43 -0600

    array project 4/23/22

---
+++ LearnJS/MagicEightBall/js/main.js
@@ -0,0 +1,63 @@
+const userName = '';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = '';
+console.log(`Will ${userName} die?`);
+
+const randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+if(randomNumber === 0){
+  console.log('It is certain');
+}else if(randomNumber === 1){
+  console.log('It is decidedly so');
+}else if(randomNumber === 2){
+  console.log('Reply hazy try again');
+}else if(randomNumber === 3){
+  console.log('Cannot predict now');
+}else if(randomNumber === 4){
+  console.log('Do not count on it');
+}else if(randomNumber === 5){
+  console.log('My sources say no');
+}else if(randomNumber === 6){
+  console.log('Outlook not so good');
+}else if(randomNumber === 7){
+  console.log('Signs point to yes');
+};
+
+
+
+
+
+
+
+
+
+
+
+// switch (eightBall) {
+//   case 0:
+//     console.log('It is certain');
+//     break;
+//   case 1:
+//     console.log('It is decidedly so');
+//     break;
+//   case 2:
+//     console.log('Reply hazy try again');
+//     break;
+//   case 3:
+//     console.log('Cannot predict now');
+//     break;
+//   case 4:
+//     console.log('Do not count on it');
+//     break;
+//   case 5:
+//     console.log('My sources say no');
+//     break;
+//   case 6:
+//     console.log('Outlook not so good');
+//     break;
+//   case 7:
+//     console.log('Signs point to yes');
+//     break;
+//     }
